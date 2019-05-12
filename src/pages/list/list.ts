f8642fc6a5376ca4c0ab6c3e9cb6e109a20909d8
thiyanesh <thiyanesh84@outlook.com>
2019-05-12 20:28:42 +0300

    view notifications design inprogress

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams  } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
 
 @Component({
@@ -11,7 +11,7 @@ export class ListPage {
   // should be each tab's root Page
   selectedItem: any;
   icons: string[];
-  items: Array<{id: string, entity: string, timestamp: string, sentTo: string, url: string, title: string, note: string, icon: string}>;
+  items: Array<{ id: string, entity: string, timestamp: string, sentTo: string, url: string, title: string, note: string, icon: string }>;
 
   //notification
 
@@ -22,18 +22,18 @@ export class ListPage {
 
     // Let's populate this page with some filler content for funzies
     this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+      'american-football', 'boat', 'bluetooth', 'build'];
 
     this.items = [];
     for (let i = 1; i < 20; i++) {
       this.items.push({
-        id: i+'',
+        id: i + '',
         entity: 'General Directorate of Traffic',
         timestamp: '20/jan/2019',
         sentTo: '+973-1111111, +973-323232323, test@test.com, test2@test.com',
         url: 'https://www.google.com/',
         title: 'Notification Title ' + i,
-        note: 'This is notification description for notification ' + i,
+        note: 'This is notification description for notification ' + i + ': These services, provided by Information & eGovernment Authority (IGA), allow the citizens, residents and GCC citizens to register and update their contact details which will be used to receive latest government notifications with the ability to view these notifications via the e-service.',
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
     }
@@ -46,7 +46,7 @@ export class ListPage {
     });
   }
 
-  notificationView(event:any, item: any){
+  notificationView(event: any, item: any) {
     console.log(item);
     this.navCtrl.push(TabsPage, {
       item: item
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -16,28 +16,12 @@ import { ListPage } from '../list/list';
   templateUrl: 'tabs.html',
 })
 export class TabsPage {
-  item: {id: string, entity: string, timestamp: string, sentTo: string, url: string, title: string, note: string, icon: string};
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  notification: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {    
+    this.notification = this.navParams.data.item;
   }
-  entity: any = ListPage;
-  // set some user information on chatParams
-  entityParams = {
-    orderby: 'entity'
-  };
   ionViewDidLoad() {
     console.log('ionViewDidLoad TabsPage');
-    console.log(this.navParams.data.item.id);
-    this.item = {
-      id: this.navParams.data.item.id,
-        entity: this.navParams.data.item.entity,
-        timestamp: this.navParams.data.item.timestamp,
-        sentTo: this.navParams.data.item.sentTo,
-        url: this.navParams.data.item.url,
-        title: this.navParams.data.item.title,
-        note: this.navParams.data.item.note,
-        icon: this.navParams.data.item.icon
-    }
   }
 
 }
